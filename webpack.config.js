5f78c17a18454f0086a1eef32df2d165fdb5ee48
Hunter Rancourt <skwid138@gmail.com>
2017-12-08 21:02:56 -0600

    setup webpack loader to use babel for transpiling es5 to es6

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,16 @@ var webpackConfig = {
     filename: "index.js"
   },
   module: {
-    loaders: []
+    loaders: [
+      {
+        // the babel loader will transpile es5 into es2015
+        loader: "babel-loader",
+        // this expression hits all files ending in .js
+        test: /.js$/,
+        // this wont transpile anything in node_modules
+        exclude: /node_modules/
+      }
+    ]
   }
 };
 
